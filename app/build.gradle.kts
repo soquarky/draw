0ab6fae1b2376d0ec2bb1cc3d88b26cb73aa9f5e
steven owens <adventuresindrawing@gmail.com>
2026-03-02 08:59:34 -0800

    Add dependencies and Kotlin serialization plugin

---
+++ app/build.gradle.kts
@@ -0,0 +1,10 @@
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
+    implementation("com.caverock:androidsvg:1.4")
+    implementation("androidx.documentfile:documentfile:1.0.1")
+}
+
+plugins {
+    id("org.jetbrains.kotlin.plugin.serialization")
+}
